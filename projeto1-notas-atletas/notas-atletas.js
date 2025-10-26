2eb22533acfa8cff3540870df952570efe35eda0
paulomoura24 <paulomouramoura1412@gmail.com>
2025-10-25 21:32:28 -0300

    projeto1-notas-atletas

---
+++ projeto1-notas-atletas/notas-atletas.js
@@ -0,0 +1,42 @@
+let atletas = [
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas: [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5]
+  }
+];
+
+function calcularmedia(notas) {
+    let notasOrdenadas = notas.sort((a, b) => a - b);
+
+    let notasComputadas = notasOrdenadas.slice(1, 4);
+
+    let soma = 0;
+    notasComputadas.forEach(function(nota) {
+        soma += nota;
+    });
+
+    let media = soma / notasComputadas.length;
+    return media;
+}
+
+for (let i = 0; i < atletas.length; i++) {
+    let atleta = atletas[i];
+    let media = calcularmedia(atleta.notas);
+   
+    console.log(`Atleta: ${atleta.nome}`);
+    console.log(`Notas Obtidas: ${atleta.notas.join(", ")}`);
+    console.log(`Média Válida: ${media}`);
+    console.log("");
+}
